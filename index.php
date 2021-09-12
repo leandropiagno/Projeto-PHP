bd54efd80cbc91e5a6714c8273c71c7808c8a5b3
Leandro Piagno <leandropiagno1984@gmail.com>
2021-09-12 17:07:04 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="author" content="">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fomulário de inscrição</title>
+</head>
+<body>
+    <p>FORMULÁRIOS PARA INSCRIÇÕES DE COMPETIDORES</p>
+
+    <FORM action="script.php" method="post">
+        <p>Seu nome: <input type="text" name="nome"></p>
+        <p>Sua idade: <input type="text" name="idade"></p>
+        <p><input type="submit" value="Enviar dados do competidor"></p>
+    </FORM>
+
+</body>
+</html>
